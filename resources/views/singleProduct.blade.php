d7b13b4d11293e9b0e24ac63a9c485d7b1c80d95
AhmedMohammedKhalil <a.m.k.y.2013@gmail.com>
2023-04-23 04:48:38 +0200

    some changes in images

--- resources/views/singleProduct.blade.php
+++ resources/views/singleProduct.blade.php
@@ -1,5 +1,51 @@
 @extends('layouts.app')
+@push('css')
+<style>
+    .pt-product .pt-description .pt-title a {
+        font-size: 18px;
+        font-weight: bold !important;
+    }
 
+    .pt-img img, .pt-img-roll-over img {
+        width:100% !important;
+        height: 316px !important;
+    }
+
+    #smallGallery a , #smallGallery img{
+        width:100% !important;
+        height: 65px !important;
+    }
+
+    .zoom-product{
+        width: 100% !important;
+        height: 372px !important;
+    }
+
+    .pt-app-btn {
+        background: white;
+        padding:5px;
+        border:1px solid black;
+        border-radius:10px
+    }
+
+    .pt-product:hover .pt-app-btn{
+        opacity: 90% !important;
+    }
+
+    li span {
+        display: inline-block;
+    }
+
+    .pt-col:first-child {
+        flex:none
+    }
+
+    .pt-col {
+        flex:auto
+    }
+
+</style>
+@endpush
 @section('breadcrumb')
 <div class="pt-breadcrumb">
     <div class="container-fluid">
